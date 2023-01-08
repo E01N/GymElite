979326b9de1dea475dc446e70b7353f8885b8105
Eoin Murphy <97413778+E01N@users.noreply.github.com>
2023-01-08 17:05:10 +0000

    added path to templates

--- project_5/settings.py
+++ project_5/settings.py
@@ -68,7 +68,10 @@ ROOT_URLCONF = 'project_5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
