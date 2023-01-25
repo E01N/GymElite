a523b8158da019dde59e98775c19ffc9b5bef9c9
Eoin Murphy <murphy.eoin93@gmail.com>
2023-01-25 18:04:24 +0000

    removed blank line

--- project_5/urls.py
+++ project_5/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
