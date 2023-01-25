dc31dd5b3abfbc6d161427668a58e1c93a95b357
Eoin Murphy <murphy.eoin93@gmail.com>
2023-01-25 18:04:06 +0000

    added blank line

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
+
+
 def all_products(request):
     """ A view to show all products, including sorting and search """
 
