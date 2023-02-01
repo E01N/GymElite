87669e9311c9d34532b03aa835582a53e0efbcbe
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-01 13:41:38 +0000

    added toasts

--- bag/views.py
+++ bag/views.py
@@ -23,6 +23,8 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
     
     request.session['bag'] = bag
     print(request.session['bag'])
