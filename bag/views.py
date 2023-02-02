45ed49927f2d041276f7c84b485cf34fcfec0e32
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-02 17:14:40 +0000

    added sizes to cart (not working)

--- bag/views.py
+++ bag/views.py
@@ -17,7 +17,11 @@ def add_to_bag(request, item_id):
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.session.get('size')
     bag = request.session.get('bag', {})
+
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
@@ -26,11 +30,11 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
