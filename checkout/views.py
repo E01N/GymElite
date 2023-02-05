d9b16a08e12c56a6509b4420435665850930c7ef
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-05 20:35:24 +0000

    stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MYAZSB7wxoQRYe9J5kXNaJRMocJwMqazCqiZRIDncw9NJiPeguTq9g2qxNdsy6CyakMK9bFLQd3qNIOXLk7rBXV00g4dxFK2q',
+        'client_secret': 'sk_test_51MYAZSB7wxoQRYe9Xd8zjB8L4OD1qNOvYMl875bOTiLIH74RnVwfNeyPUls9vH5uxVWSX9TGJV5cr8oc7XlnS6TG004B0DbSeb',
     }
 
     return render(request, template, context)
