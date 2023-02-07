c339c4f1ed73987e793da52412108a52703f1e0f
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-07 18:58:38 +0000

    removed diet_plan. will replace w/ better version

--- diet_plan/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('create_user/', views.create_user, name='create_user'),
-    path('create_meal/<int:user_id>/', views.create_meal, name='create_meal'),
-]
