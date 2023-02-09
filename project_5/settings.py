0e3a6f4fef6be0601d05b7f3923d1788c79f6816
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-09 01:26:58 +0000

    removed secret key and set debug

--- project_5/settings.py
+++ project_5/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY = 'django-insecure-=3r=9j)k9q$&=8()o96=0b77#^-o3-9r7ha=-h-p6@car^7rd8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
