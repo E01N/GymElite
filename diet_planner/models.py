98e5912c5cd0f72fc200524cdf0d055137d0683e
Eoin Murphy <murphy.eoin93@gmail.com>
2023-03-27 16:50:00 +0000

    added gender choices

--- diet_planner/models.py
+++ diet_planner/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class UserProfile(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
     age = models.PositiveIntegerField()
     weight = models.PositiveIntegerField()
     height = models.PositiveIntegerField()
@@ -10,3 +15,4 @@ class UserProfile(models.Model):
     dietary_restriction = models.CharField(max_length=50)
     goal = models.CharField(max_length=50)
     workout_goal = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='O')
