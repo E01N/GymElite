7a6998cf38a12ac339fbd7ff3bb7aa800cb9df11
Eoin Murphy <murphy.eoin93@gmail.com>
2023-04-10 11:25:56 +0000

    setting up secret key correctly

--- project_5/settings.py
+++ project_5/settings.py
@@ -14,6 +14,8 @@ import dj_database_url
 
 from pathlib import Path
 
+from .env import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'TRUE'
